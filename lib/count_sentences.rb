0fbbd6ae24ff6bc6fe16a041b48aa1735b3d99a4
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-02-17 00:15:21 -0600

    commit tree ae8b66f44606208b3b449cdbac10227a33820273

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter {|sentence| !sentence.empty?}.size
   end
 end
+
+puts ("Hi, my name is sophie").count_sentences
+puts ("Hey. What is up? I am good!").count_sentences
